bf76d10b1fea24ea378ba766f2c852893446ba9f
annnikolaeva <117666686+annnikolaeva@users.noreply.github.com>
2022-11-07 19:56:33 +0300

    Update Rotation_tensor.m

--- Rotation_tensor.m
+++ Rotation_tensor.m
@@ -1,6 +1,8 @@
-%поворот тензора 
+% RR - initial tensor
+% (x,y,z) - the axis around which the rotation through the angle beta, x^2+y^2+z^2=1
+% XX - rotated tensor
 function XX=Rotation_tensor(x,y,z,beta,RR)
-MM=Mmatrix(x,y,z,beta);
+MM=Mmatrix(x,y,z,beta); % rotation matrix, Mmatrix - rotation function
 MM1=inv(MM);
 XX=zeros(3, 3, 3);
 for i=1:3
@@ -10,7 +12,6 @@ for a=1:3
     for b=1:3
         for c=1:3    
             XX(i,j,k)=XX(i,j,k)+MM(k,c)*MM(j,b)*MM(i,a)*RR(a,b,c);
-            %XX(i,j,k)=XX(i,j,k)+MM(c,k)*MM(b,j)*MM(a,i)*RR(a,b,c);
         end
     end
 end
